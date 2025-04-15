97fbf0038e8309e36f85979daf2fa18849c6f5eb
Alexandry <ale_and5@hotmail.com>
2025-04-15 18:36:27 -0300

    melhoria no codigo e inclusao de rotas

--- run.py
+++ run.py
@@ -1,58 +1,6 @@
-from flask import Flask, jsonify
-from app.services import embrapa_service
-import requests
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/produtos')
-def produtos():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
-    response = requests.get(url)
-    response.encoding = 'utf-8'
-    html = response.text
-
-    dados = embrapa_service.extrair_dados_tabela(html)
-    return jsonify(dados)
-
-@app.route('/processamento')
-def processamento():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_03'
-    response = requests.get(url)
-    response.encoding = 'utf-8' 
-    html = response.text
-
-    dados = embrapa_service.extrair_dados_tabela(html)
-    return jsonify(dados)
-
-@app.route('/comercializacao')
-def comercializacaoento():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
-    response = requests.get(url)
-    response.encoding = 'utf-8' 
-    html = response.text
-
-    dados = embrapa_service.extrair_dados_tabela(html)
-    return jsonify(dados)
-
-@app.route('/importacao')
-def importacao():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05'
-    response = requests.get(url)
-    response.encoding = 'utf-8' 
-    html = response.text
-
-    dados = embrapa_service.extrair_exportacao_importacao(html)
-    return jsonify(dados)
-
-@app.route('/exportacao')
-def exportacao():
-    url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_06'
-    response = requests.get(url)
-    response.encoding = 'utf-8' 
-    html = response.text
-
-    dados = embrapa_service.extrair_exportacao_importacao(html)
-    return jsonify(dados)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
